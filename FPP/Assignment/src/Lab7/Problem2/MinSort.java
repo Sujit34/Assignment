e87c8984a7767ff83b47fd9a6aed744d5116599c
sujit34 <sujitcsecuet@gmail.com>
2022-09-27 23:39:11 -0500

    FPP Lab7

--- FPP/Assignment/src/Lab7/Problem2/MinSort.java
+++ FPP/Assignment/src/Lab7/Problem2/MinSort.java
@@ -49,6 +49,49 @@ public class MinSort {
  
         return sb.toString();
     }
+	
+	/*
+	 public String sort(String str){
+		if(str == null || str.length() <= 0) return "";		
+		
+		int len = str.length();
+		char[] strArr = str.toCharArray();
+		
+		//Swapping the 1st character with the minimum one
+		swap(strArr, 0, minpos(strArr, 0,len-1));
+		
+		//Calling the method recursively for rest of the characters
+		String result = strArr[0] + this.sort(String.copyValueOf(strArr, 1, len-1));
+		
+		return result;
+	}
+	
+	void swap(char[] strArray, int i, int j){
+		char temp = strArray[i];
+		strArray[i] = strArray[j];
+		strArray[j] = temp;
+		
+	}
+	//find minimum of arr between the indices bottom and top
+	public int minpos(char[] strArr, int bottom, int top){		
+		int m = strArr[bottom];
+		int index = bottom;
+		for(int i = bottom+1; i <= top; ++i){
+			if(strArr[i] < m){ 
+				m = strArr[i];
+				index = i;
+			}
+		}
+		//return location of min, not the min itself
+		return index;
+	}
+
+	public static void main(String[] args) {
+		MinSort ms = new MinSort();
+		String result = ms.sort("zwxuabfkafutbbbb");
+		System.out.println(result);
+	}
+	 */
 
 
 }
